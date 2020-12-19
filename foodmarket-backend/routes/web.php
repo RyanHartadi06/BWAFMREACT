f4d15578eafbf9d6e28375463c93b0e6b9c881fa
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-12-19 17:29:32 +0700

    Create Data

--- foodmarket-backend/routes/web.php
+++ foodmarket-backend/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserControllers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +15,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Home page
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
+
+//Dashboard
+
+Route::prefix('dashboard')
+    ->middleware(['auth:sanctum' , 'admin'])
+    ->group(function(){
+        Route::get('/', [DashboardController::class , 'index'])->name('dashboard');
+        Route::resource('users', UserControllers::class);
 });
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+
 
 Route::get('midtrans/success', [MidtransController::class, 'success']);
 Route::get('midtrans/unfinish', [MidtransController::class, 'unfinish']);
