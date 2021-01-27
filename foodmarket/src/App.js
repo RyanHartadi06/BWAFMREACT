485bcaac8519a246aac11fa3aff0384ca3799df8
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-01-27 19:29:44 +0700

    Redux

--- foodmarket/src/App.js
+++ foodmarket/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native'
+import {NavigationContainer} from '@react-navigation/native';
 import Router from './router';
-
+import {Provider} from 'react-redux';
+import store from './redux/store';
 const App = () => {
   return (
     <NavigationContainer>
-      <Router />
+      <Provider store={store}>
+        <Router />
+      </Provider>
     </NavigationContainer>
-  )
-  
-}
+  );
+};
 
-export default App
+export default App;
--- foodmarket/src/pages/SignUp/index.js
+++ foodmarket/src/pages/SignUp/index.js
@@ -1,61 +1,71 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Header , Button, Gap , TextInput} from '../../components'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Header, Button, Gap, TextInput} from '../../components';
+import {useSelector} from 'react-redux';
 
 const SignUp = ({navigation}) => {
-    return (
-        <View style={styles.page}>
-           <Header title="Sign Up" subtitle="Register and eat" onBack={() => {}} />
-           <View style={styles.container}>
-                <View style={styles.photo}>
-                    <View style={styles.borderPhoto}>
-                        <View style={styles.photoContainer}>
-                            <Text style={styles.addphoto}>Add Photo</Text>
-                        </View>
-                    </View>
-                </View>
-                <TextInput label="Full Name" pl="Type your fullnname"/>
-                <Gap height={16}/>
-                <TextInput label="Email Address" pl="Type your email address"/>
-                <Gap height={16}/>
-                <TextInput label="Password" pl="Type your password"/>
-                <Gap height={24}/>
-                <Button title="Continue" onPress={() => navigation.navigate('SignUpAddress')}/>
-               
+  const globalState = useSelector((state) => state.globalReducer);
+  console.log(globalState);
+  return (
+    <View style={styles.page}>
+      <Header title="Sign Up" subtitle="Register and eat" onBack={() => {}} />
+      <View style={styles.container}>
+        <View style={styles.photo}>
+          <View style={styles.borderPhoto}>
+            <View style={styles.photoContainer}>
+              <Text style={styles.addphoto}>Add Photo</Text>
             </View>
+          </View>
         </View>
-    )
-}
+        <Text>{`state : ${globalState.isError}`}</Text>
+        <TextInput label="Full Name" pl="Type your fullnname" />
+        <Gap height={16} />
+        <TextInput label="Email Address" pl="Type your email address" />
+        <Gap height={16} />
+        <TextInput label="Password" pl="Type your password" />
+        <Gap height={24} />
+        <Button
+          title="Continue"
+          onPress={() => navigation.navigate('SignUpAddress')}
+        />
+      </View>
+    </View>
+  );
+};
 
-export default SignUp
+export default SignUp;
 
 const styles = StyleSheet.create({
-    page : {flex:1}, 
-    container : {
-        backgroundColor : 'white' , 
-        paddingHorizontal : 24 , 
-        paddingVertical : 26 ,
-        marginTop : 24 , 
-        flex : 1
-    },
-    photoContainer : {
-        width : 90 , height : 90 , borderRadius : 90 , backgroundColor : '#F0F0F0' , padding : 24 
-    }, 
-    addphoto :{ 
-        fontSize : 14,
-        fontFamily : 'Poppins-Light',
-        color : '#8D92A3',
-        textAlign : 'center'
-    },
-    borderPhoto :{ 
-        borderWidth : 1,
-        borderColor : '#8D92A3',
-        width : 110,
-        height : 110,
-        borderRadius : 110,
-        borderStyle : 'dashed', 
-        justifyContent : 'center' , 
-        alignItems : 'center'
-    },
-    photo : {alignItems : 'center' , marginTop : 26 , marginBottom : 16}
-})
+  page: {flex: 1},
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingVertical: 26,
+    marginTop: 24,
+    flex: 1,
+  },
+  photoContainer: {
+    width: 90,
+    height: 90,
+    borderRadius: 90,
+    backgroundColor: '#F0F0F0',
+    padding: 24,
+  },
+  addphoto: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Light',
+    color: '#8D92A3',
+    textAlign: 'center',
+  },
+  borderPhoto: {
+    borderWidth: 1,
+    borderColor: '#8D92A3',
+    width: 110,
+    height: 110,
+    borderRadius: 110,
+    borderStyle: 'dashed',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  photo: {alignItems: 'center', marginTop: 26, marginBottom: 16},
+});
